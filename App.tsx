8246cbe93a05f27e1fea186e7cd6beca414d6197
Jonata Serpa <jonataserpa@gmail.com>
2026-04-28 08:50:29 -0300

    feat: add root app component

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StatusBar, useColorScheme} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {AppNavigator} from './src/app/AppNavigator';
+
+export default function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppNavigator />
+    </SafeAreaProvider>
+  );
+}
